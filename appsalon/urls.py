dfb360baf6d36e407ec786ce24b1c25d95b0c5ce
Juan Martinez <pablomar01@outlook.com>
2024-07-01 13:25:49 -0500

    create serializer, viewset and endpoints for services

--- appsalon/urls.py
+++ appsalon/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from services.views import ServiceViewSet
+
+router = routers.DefaultRouter()
+router.register(r'services', ServiceViewSet, basename='service')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- services/views.py
+++ services/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ServiceSerializer
+from .models import Service
 
 # Create your views here.
+class ServiceViewSet(viewsets.ModelViewSet):
+    serializer_class = ServiceSerializer
+    queryset = Service.objects.all()
