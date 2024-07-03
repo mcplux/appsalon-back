4b1656223ea6f5e4788023ebaaa0f729de5d6597
Juan Martinez <pablomar01@outlook.com>
2024-07-02 19:23:25 -0500

    chore: Configure email sending settings and frontend URL

--- appsalon/settings.py
+++ appsalon/settings.py
@@ -143,3 +143,14 @@ if DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True
 
 AUTH_USER_MODEL = 'users.User'
+
+# Email sending settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+# Frontend url
+FRONTEND_URL = os.getenv('FRONTEND_URL')
--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 from rest_framework import generics, status
 from rest_framework.response import Response
 from .serializers import UserRegisterSerializer
+from .models import Token
 
 # Create your views here.
 class UserRegisterView(generics.CreateAPIView):
@@ -11,4 +16,22 @@ class UserRegisterView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+        try:
+            token = Token.objects.get(user=user)
+            url = f'{settings.FRONTEND_URL}/auth/verify-account/{token.token}' # Frontend verify account url
+
+            # Send email with token
+            subject = 'AppSalon - Verify your account'
+            html_content = render_to_string('users/verify_account.html', { 'url': url })
+            text_content = strip_tags(html_content)
+            email = EmailMultiAlternatives(subject, text_content, 'admin@appsalon.com', [user.email])
+            email.attach_alternative(html_content, 'text/html')
+            email.send()
+
+        except Exception as e:
+            print(e)
+            user.delete()
+            return Response({ 'message': 'An error ocurred, try again later' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
         return Response({ 'message': 'User created succesfully' })
