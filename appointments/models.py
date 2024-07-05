03e02b903e2089b03ecafaf8f924eaadbf7a8d6c
Juan Martinez <pablomar01@outlook.com>
2024-07-04 20:35:25 -0500

    feat: Add Appointment model and migration

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from services.models import Service
+from users.models import User
 
 # Create your models here.
+class Appointment(models.Model):
+    services = models.ManyToManyField(Service, related_name='appointments')
+    date = models.DateTimeField()
+    time = models.CharField(max_length=10)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointments')
+
+    def __str__(self):
+        return f'{self.user.first_name} - {self.date} - {self.time}'
