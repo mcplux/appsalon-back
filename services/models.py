346681150c5ad5c00f950e556de6b112b75eb611
Juan Martinez <pablomar01@outlook.com>
2024-07-05 11:50:42 -0500

    refactor: Add __str__ method to Service model

--- services/models.py
+++ services/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Service(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
     
