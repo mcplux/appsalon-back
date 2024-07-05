110d20475a6a2b945a083e315d895aabee7e6636
Juan Martinez <pablomar01@outlook.com>
2024-07-05 11:49:36 -0500

    feat: Add AppointmentAdmin to manage appointments in the admin interface

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Appointment
 
 # Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'time', 'total_amount')
+    search_fields = ('user__first_name', 'date', 'time')
+    list_filter = ('date', 'time')
+    
