5ef725aa63dda187a5d6479e6fcaa7387b62ba96
Juan Martinez <pablomar01@outlook.com>
2024-07-12 14:06:35 -0500

    chore: Update appointment serializer to include service objects

--- appointments/serializer.py
+++ appointments/serializer.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Appointment
+from services.serializers import ServiceSerializer
 
 class AppointmentSerializer(serializers.ModelSerializer):
+    services = ServiceSerializer(many=True, read_only=True)
     class Meta:
         model = Appointment
         fields = ('id', 'user', 'date', 'time', 'total_amount', 'services')
